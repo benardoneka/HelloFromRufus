023af808d46107dd3d2957d3d1800928b6700131
Benard <68639840+benardoneka@users.noreply.github.com>
2021-05-24 12:34:56 +0300

    Update HelloFromRufus.java
    I have I have added a line of code to your project

--- src/HelloFromRufus.java
+++ src/HelloFromRufus.java
@@ -2,5 +2,6 @@ public class HelloFromRufus {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Rufus");
+        System.out.println("Hello Brother Rufus, this is Benard from the Group.");
     }
 }
